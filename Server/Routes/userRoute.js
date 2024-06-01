54fd53bb81150128d45fb55ff6062ce1e12ce2ef
Deevyne99 <kaludivine545@gmail.com>
2024-06-01 11:25:52 +0100

    Build: Built user controller and routes

--- Server/Routes/userRoute.js
+++ Server/Routes/userRoute.js
@@ -1,7 +1,20 @@
 const express = require('express')
-const { getAllUsers } = require('../Controllers/userController')
+const {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+} = require('../Controllers/userController')
+const AuthenticateUSer = require('../Middleware/AuthenticateUser')
+const AuthorizeUser = require('../Middleware/AuthorizeUser')
 const router = express.Router()
 
-router.route('/user').get(getAllUsers)
+router.route('/').get(AuthenticateUSer, AuthorizeUser('Admin'), getAllUsers)
+
+router.route('/showMe').get(AuthenticateUSer, showCurrentUser)
+router.route('/updateUser').patch(AuthenticateUSer, updateUser)
+router.route('/:id').get(AuthenticateUSer, getSingleUser)
+
+router.route
 
 module.exports = router
