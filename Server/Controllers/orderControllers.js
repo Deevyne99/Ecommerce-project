a20c03bd9cc6627f63f47563bcd3476d305ea97e
Deevyne99 <kaludivine545@gmail.com>
2024-06-25 16:04:34 +0100

    Build: built order Controller

--- Server/Controllers/orderControllers.js
+++ Server/Controllers/orderControllers.js
@@ -1,5 +1,13 @@
 const customApiErrors = require('../Errors')
 const { StatusCodes } = require('http-status-codes')
+const Products = require('../Models/products')
+const Order = require('../Models/Order')
+const { checkPermission } = require('../Utils/')
+
+const fakeStripeAPI = async ({ amount, currency }) => {
+  const client_secret = 'someRandomValue'
+  return { client_secret, amount }
+}
 
 const createOrder = async (req, res) => {
   const { items: cartItems, tax, shippingFee } = req.body
@@ -14,14 +22,61 @@ const createOrder = async (req, res) => {
   }
 
   let orderItems = []
+  let subtotal = 0
+  for (const item of cartItems) {
+    const dBProducts = await Products.findOne({ _id: item.product })
+    if (!dBProducts) {
+      throw new customApiErrors.NotFoundError(
+        `No product with id : ${item.product}`
+      )
+    }
+
+    const { name, price, image, _id } = dBProducts
+    const singleOrderitem = { amount: item.amount, name, price, image, _id }
+    //add item to orderItem array
+    orderItems = [...orderItems, singleOrderitem]
+    subtotal = item.amount * price
+  }
+
+  // calculate total
+  const total = tax + shippingFee + subtotal
+  // get client secret
+  const paymentIntent = await fakeStripeAPI({
+    amount: total,
+    currency: 'usd',
+  })
+
+  const order = await Order.create({
+    orderItems,
+    total,
+    subtotal,
+    tax,
+    shippingFee,
+    clientSecret: paymentIntent.client_secret,
+    user: req.user.userId,
+  })
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ order, clientSecret: order.clientSecret })
 }
 
 const getAllOrders = async (req, res) => {
-  res.send('get all orders')
+  const orders = await Order.find({})
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, orders, count: orders.length })
 }
 
 const getSingleOrder = async (req, res) => {
-  res.send('get single order')
+  const { id: orderId } = req.params
+  const order = await Order.findOne({ _id: orderId })
+  if (!order) {
+    throw new customApiErrors.NotFoundError(`No order with id : ${orderId}`)
+  }
+  checkPermission(req.user, order.user)
+
+  res.status(StatusCodes.OK).json({ success: true, order })
 }
 
 const deleteOrder = async (req, res) => {
@@ -33,7 +88,8 @@ const updateOrder = async (req, res) => {
 }
 
 const getCurrentUserOrder = async (req, res) => {
-  res.send('get current user order')
+  const orders = await Order.find({ user: req.user })
+  res.status(StatusCodes.OK).json({ success: true, orders })
 }
 
 module.exports = {
