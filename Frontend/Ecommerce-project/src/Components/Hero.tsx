922b9586aa23e884b4622bea88cb614331d52fe0
Deevyne99 <kaludivine545@gmail.com>
2024-08-11 21:15:34 +0100

    Build: Added lazy Loading to hero component

--- Frontend/Ecommerce-project/src/Components/Hero.tsx
+++ Frontend/Ecommerce-project/src/Components/Hero.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react'
 import { products } from '../data'
 import { Link } from 'react-router-dom'
+import { Blurhash } from 'react-blurhash'
 
 const Hero = () => {
-  // const [items, setItems] = useState(products)
   const [productIndex, setProductIndex] = useState(0)
+  const [loaded, setLoaded] = useState(Array(products.length).fill(false))
 
   useEffect(() => {
     const lastIndex = products.length - 1
@@ -23,11 +24,19 @@ const Hero = () => {
     return () => clearInterval(slider)
   }, [productIndex])
 
+  const handleImageLoad = (index: number) => {
+    setLoaded((prevLoaded) => {
+      const newLoaded = [...prevLoaded]
+      newLoaded[index] = true
+      return newLoaded
+    })
+  }
+
   return (
-    <section className='md:mt-12 mt-4 h-[500px] md:h-screen relative w-full flex flex-col overflow-hidden'>
+    <section className='md:mt-12 mt-4 h-[550px] md:h-screen relative w-full flex flex-col overflow-hidden'>
       <div className='flex justify-center items-center gap-8 relative w-full h-full'>
-        {products.map((item, index: number) => {
-          const { id, title, img, desc } = item
+        {products.map((item, index) => {
+          const { id, title, img, desc, blurhash } = item
           let position = 'translate-x-full opacity-0'
 
           if (productIndex === index) {
@@ -42,15 +51,30 @@ const Hero = () => {
           return (
             <div
               key={id}
-              className={`absolute    top-0 left-0 w-full h-full flex flex-col  md:flex-row items-center justify-between md:gap-12 gap-4 transition-transform duration-300 ${position}`}
+              className={`absolute top-0 left-0 w-full h-full flex flex-col md:flex-row items-center justify-between md:gap-12 gap-4 transition-transform duration-300 ${position}`}
             >
-              <img
-                src={img}
-                alt={title}
-                className='md:h-full h-[300px] w-full  md:w-1/2 object-cover '
-              />
+              <div className='md:w-1/2 w-full h-full'>
+                {!loaded[index] && (
+                  <Blurhash
+                    hash={blurhash}
+                    width='100%'
+                    height='100%'
+                    resolutionX={32}
+                    resolutionY={32}
+                    punch={1}
+                    className='object-cover h-full w-full'
+                  />
+                )}
+                <img
+                  src={img}
+                  alt={title}
+                  onLoad={() => handleImageLoad(index)}
+                  style={{ display: loaded[index] ? 'block' : 'none' }}
+                  className='object-cover h-full w-full'
+                />
+              </div>
               <div className='flex flex-col items-center justify-center gap-2 w-full md:w-1/2 max-w-[600px]'>
-                <h2 className='font-bold text-2xl text-[#1c0f0f]  md:text-left text-center '>
+                <h2 className='font-bold text-2xl text-[#1c0f0f] md:text-left text-center'>
                   {title}
                 </h2>
                 <p className='md:w-[80%] w-full text-center text-[#6b7280] text-[15px] px-4 md:px-0 leading-8'>
