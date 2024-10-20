1a494eeb9b40cc55996ee091314416b9039c80a3
Deevyne99 <kaludivine545@gmail.com>
2024-10-20 21:43:38 +0100

    Build:building checkout page

--- Frontend/Ecommerce-project/src/Pages/Checkout.tsx
+++ Frontend/Ecommerce-project/src/Pages/Checkout.tsx
@@ -1,7 +1,58 @@
+import { RiDeleteBin6Fill } from 'react-icons/ri'
+import { useAppSelector, useAppDispatch } from '../hooks/hooks'
+import { formatPrice } from '../utils'
+import { removeItem } from '../features/cart/cartslice'
+
 // import React from 'react'
 
 const Checkout = () => {
-  return <div className='mt-24'>Checkout</div>
+  const { cartItems } = useAppSelector((state) => state.cartSlice)
+  const dispatch = useAppDispatch()
+  return (
+    <section className='mt-20 flex flex-col mx-4 md:mx-16  items-center '>
+      <div className='flex justify-between  w-full'>
+        <div className='flex flex-col w-[50%] gap-6  py-8 px-4'>
+          <h2 className='capitalize text-2xl'>your order</h2>
+          <div className='flex flex-col gap-8 border rounded-md p-4'>
+            {cartItems.map((item) => {
+              const { product, image, name, amount, price } = item
+              return (
+                <div
+                  key={product}
+                  className='flex gap-2 border p-4 rounded-md items-center justify-between'
+                >
+                  <div className='w-[60px] h-[60px] md:w-[90px] md:h-[90px]'>
+                    <img
+                      src={image}
+                      alt=''
+                      className='w-full h-full justify-center items-center object-cover object-center'
+                    />
+                  </div>
+                  <article className='w-[80%] md:max-w-[350px] flex flex-col gap-1  text-[#6b7280]'>
+                    <p>{name}</p>
+
+                    <p className='text-[#3b82f6] font-bold text-sm'>
+                      {amount} x {formatPrice(price)}
+                    </p>
+                  </article>
+                  <button
+                    onClick={() => dispatch(removeItem({ product, amount }))}
+                    className='flex'
+                  >
+                    <RiDeleteBin6Fill className='text-xl text-red-500' />
+                  </button>
+                </div>
+              )
+            })}
+          </div>
+          <div>
+            <h2>Order Summary</h2>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  )
 }
 
 export default Checkout
