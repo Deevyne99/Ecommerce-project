a052d510994f9a386cde8a35ee1a13fc50d0b89c
Deevyne99 <kaludivine545@gmail.com>
2024-11-03 19:48:26 +0100

    Fixed bug

--- Server/app.js
+++ Server/app.js
@@ -27,12 +27,7 @@ const app = express()
 
 //+++++++++++++++++++++++++++++++Middleware++++++++++++++++++++++++++++++++++++++++
 app.use(helmet())
-app.use(
-  cors({
-    origin: 'http://localhost:5173/',
-    credentials: true,
-  })
-)
+app.use(cors())
 app.use(xss())
 
 app.set('trust proxy', 1)
