6741ba59e22a112f588cb6f14eb0901153a7c4b7
Deevyne99 <kaludivine545@gmail.com>
2024-11-04 16:26:54 +0100

    added withCredentials to order

--- Frontend/Ecommerce-project/src/features/orders/Order.tsx
+++ Frontend/Ecommerce-project/src/features/orders/Order.tsx
@@ -43,7 +43,8 @@ export const createOrder = createAsyncThunk(
         {
           shipping,
           cartItems,
-        }
+        },
+        { withCredentials: true }
       )
       return data
     } catch (error) {
