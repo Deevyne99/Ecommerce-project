a0c88cf8b5c423c7dc4cf7c1192cf3ddc0b8b20b
Deevyne99 <kaludivine545@gmail.com>
2024-11-07 14:21:23 +0100

    added some changes

--- Server/Utils/jwt.js
+++ Server/Utils/jwt.js
@@ -19,7 +19,7 @@ const attachCookiesToResponse = ({ res, user }) => {
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
-    sameSite: 'Lax',
+    sameSite: 'none',
     domain: 'ecommerce-project-2-2n0h.onrender.com',
   })
 }
